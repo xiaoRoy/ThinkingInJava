810a983039838b58d8c955ec9af7ce8b8effdcf9
Jerry Li <xiaoroyg@163.com>
2017-12-25 23:18:10 +0800

    Learn concurrent modification.

---
+++ src/com/learn/playground/sort/concurrent/ConcurrentModification.java
@@ -0,0 +1,50 @@
+package com.learn.playground.sort.concurrent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.TimeUnit;
+
+public class ConcurrentModification {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new Vector<>();
+        numbers.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        Thread threadIterate = new Thread(() -> iterateList(numbers));
+        Thread threadAdd = new Thread(() -> addNumber(numbers));
+        threadIterate.start();
+        threadAdd.start();
+    }
+
+    private static void iterateList(List<Integer> numbers) {
+        /*
+        * acquire the the lock of vector(numbers) here,
+        * so method addNumber() numbers.add(310) can't process
+        * because it can't get the lock which acquired by this method.
+        * */
+        synchronized (numbers) {
+            System.out.println("enter");
+            for (int number : numbers) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                }
+                System.out.println(number);
+            }
+
+        }
+    }
+
+    private static void addNumber(List<Integer> numbers) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+        System.out.println("add.before");
+        numbers.add(310);
+        System.out.println("add.after");
+    }
+}
