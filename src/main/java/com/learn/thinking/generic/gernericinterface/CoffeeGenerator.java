dd558260934399f8015ba495452a5ba6d979d49c
Jerry Li <xiaoroyg@163.com>
2019-02-13 23:48:28 +0800

    Finished CoffeeGenerator

--- src/main/java/com/learn/thinking/generic/gernericinterface/CoffeeGenerator.java
+++ src/main/java/com/learn/thinking/generic/gernericinterface/CoffeeGenerator.java
@@ -3,27 +3,51 @@ package com.learn.thinking.generic.gernericinterface;
 import com.learn.thinking.generic.model.Coffee;
 
 import java.util.Iterator;
+import java.util.Random;
 
 public class CoffeeGenerator implements Generator<Coffee>, Iterable<Coffee> {
+
+    private static Random sRandom = new Random();
+
+    private int size;
+
+    private Class[] types = {Coffee.Latte.class, Coffee.Mocha.class,
+            Coffee.Cappuccino.class, Coffee.Americano.class, Coffee.Breve.class};
+
+    public CoffeeGenerator(int size) {
+        this.size = size;
+    }
+
+    public CoffeeGenerator() {
+    }
+
     @Override
     public Coffee next() {
-        return null;
+        try {
+            return (Coffee) types[sRandom.nextInt(types.length)].newInstance();
+        } catch (InstantiationException | IllegalAccessException exception) {
+            throw new RuntimeException();
+        }
     }
 
     @Override
     public Iterator<Coffee> iterator() {
-        return null;
+        return new CoffeeIterator();
     }
 
-    private static class CoffeeIterator implements Iterator<Coffee> {
+    private class CoffeeIterator implements Iterator<Coffee> {
+
+        int count = size;
+
         @Override
         public boolean hasNext() {
-            return false;
+            return count > 0;
         }
 
         @Override
         public Coffee next() {
-            return null;
+            count --;
+            return CoffeeGenerator.this.next();
         }
     }
 }
