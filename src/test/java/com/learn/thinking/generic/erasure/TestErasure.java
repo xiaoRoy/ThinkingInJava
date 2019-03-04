7aa2e22201f2dd02b7e9f32aa51fe7fd0b92c64c
Jerry Li <xiaoroyg@163.com>
2019-03-04 23:22:20 +0800

    Learned type erasure.

---
+++ src/test/java/com/learn/thinking/generic/erasure/TestErasure.java
@@ -0,0 +1,30 @@
+package com.learn.thinking.generic.erasure;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.TypeVariable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestErasure {
+
+    @Test
+    public void test_erasedTypeEquivalence() {
+        Class classOne = new ArrayList<Integer>().getClass();
+        Class classAnother = new ArrayList<String>().getClass();
+        Assert.assertEquals(classOne, classAnother);
+    }
+
+    @Test
+    public void test_typeParameters() {
+        List<Animal> animals = new ArrayList<>();
+        TypeVariable<? extends Class<? extends List>>[] typeParameters = animals.getClass().getTypeParameters();
+        Assert.assertEquals("E", typeParameters[0].toString());
+    }
+
+    private class Animal {
+
+    }
+}
