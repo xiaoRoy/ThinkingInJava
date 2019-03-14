88b45faf55ce9c58a1b7b06126c86f0cc40e242a
Jerry Li <xiaoroyg@163.com>
2019-03-14 22:58:32 +0800

    Added Generic Creator with template design pattern

---
+++ src/main/java/com/learn/thinking/generic/earsure/GenericWithCreate.java
@@ -0,0 +1,20 @@
+package com.learn.thinking.generic.earsure;
+
+public abstract class GenericWithCreate<T> {
+    private final T element;
+
+    public GenericWithCreate() {
+        element = create();
+    }
+
+    abstract T create();
+
+    private static class Phone{}
+
+    private static class PhoneCreator extends GenericWithCreate<Phone>{
+        @Override
+        Phone create() {
+            return new Phone();
+        }
+    }
+}
