650cdf33680db79401b15d5da88e49fa50c7e564
Jerry Li <xiaoroyg@163.com>
2019-05-19 22:18:53 +0800

    Learned unbound wildcards part a.

---
+++ src/main/java/com/learn/thinking/generic/boundary/UnboundWildcardsA.java
@@ -0,0 +1,50 @@
+package com.learn.thinking.generic.boundary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnboundWildcardsA {
+
+    private List rawList;
+    private List<?> unboundedList;
+    private List<? extends Number> upperBoundedList;
+
+    public void assignWithRaw(List list) {
+        rawList = list;
+        unboundedList = list;
+        upperBoundedList = list;//unchecked assignment
+    }
+
+    public void assignWithUnbounded(List<?> list) {
+        rawList = list;
+        unboundedList = list;
+//        upperBoundedList = list;
+    }
+
+    public void assignWithUpperBounded(List<? extends Number> list) {
+        rawList = list;
+        unboundedList = list;
+        upperBoundedList = list;
+    }
+
+    private void doAssign() {
+        List rawList = new ArrayList();
+        assignWithRaw(rawList);
+        assignWithUnbounded(rawList);
+        assignWithUpperBounded(rawList);//unchecked assignment
+
+        List<?> unboundedList = new ArrayList<>();
+        //You can only insert null into a List<?>.
+//        unboundedList.add(null);
+        assignWithRaw(unboundedList);
+        assignWithUnbounded(unboundedList);
+//        assignWithUpperBounded(unboundedList);
+
+        List<Integer> numbers = new ArrayList<>();
+        assignWithRaw(numbers);
+        assignWithUnbounded(numbers);
+        assignWithUpperBounded(numbers);
+
+    }
+
+}
